85df7b16b9ff945ab0884ebe37fd528672911396
Daniel <danielbetzalel16@gmail.com>
2025-01-11 23:02:09 +0200

    card ,navbar & theme

---
+++ src/Card.jsx
@@ -0,0 +1,12 @@
+
+function Card({ title, description }) {
+  return (
+    <div className="card">
+            <img className="cardImg" src="" alt="this is the first img"/>
+            <h2 className="cardTitle">first</h2>
+            <p className="cardDescription">this is the first description</p>
+    </div>
+  );
+}
+
+export default Card;
