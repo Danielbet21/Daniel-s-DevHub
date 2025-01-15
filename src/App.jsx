2c97dba571e94ade65648d42ac0edd1d1523d569
Daniel <danielbetzalel16@gmail.com>
2025-01-15 14:43:40 +0200

    logo, initial step - cards, background

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Softtag from './Softtag';
 import Hardtag from './Hardtag';
-// import Card from './Card';
+import Card from './Card';
 
 function App() {
 
@@ -14,6 +14,19 @@ function App() {
       <p>Hard Skills</p>
       <br/>
       <Hardtag />
+      <br />
+      <p>Projects</p>
+
+      <div className="cards">
+        <Card title="aa" description="this is the first description" imgRoute="src\assets\mail.png" />
+      <Card title="aa" description="this is the first description" imgRoute="src\assets\mail.png"/>
+      <Card title="aa" description="this is the first description" imgRoute="src\assets\mail.png"/>
+      <Card title="aa" description="this is the first description" imgRoute="src\assets\mail.png"/>
+      <Card title="aa" description="this is the first description" imgRoute="src\assets\mail.png"/>
+      <Card title="aa" description="this is the first description" imgRoute="src\assets\mail.png"/>
+        
+      </div>
+      
     </>
   );
 };
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,14 +1,14 @@
 
-function Card() {
+function Card({title, description, imgRoute}) {
 //   const cards = [
 //     { title: 'First Card', description: 'This is the first description' },
 //     { title: 'Second Card', description: 'This is the second description' },
 //   ];
   return (
     <div className="card">
-            <img className="cardImg" src="" alt="this is the first img"/>
-            <h2 className="cardTitle">first</h2>
-            <p className="cardDescription">this is the first description</p>
+            <img className="cardImg" src={imgRoute} alt="this is the first img"/>
+            <h2 className="cardTitle">{title}</h2>
+      <p className="cardDescription">{ description }</p>
     </div>
   );
 }
