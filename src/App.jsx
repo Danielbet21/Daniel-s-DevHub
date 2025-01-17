981f68116f95c6668a26d57ff4d6b856b1201ba1
Daniel <danielbetzalel16@gmail.com>
2025-01-17 17:22:39 +0200

    final stage

--- src/App.jsx
+++ src/App.jsx
@@ -10,18 +10,22 @@ function App() {
  
   return (
     <>
-      <p>Soft Skills</p>
-      <Softtag />
-      <br/>
-      <p>Hard Skills</p>
-      <br/>
-      <Hardtag />
+       <section id="skills">
+          <p>Soft Skills</p>
+          <Softtag />
+          <br/>
+          <p>Hard Skills</p>  
+          <br/>
+          <Hardtag />
+        </section>
       <br />
-      <p>Projects</p>
+      <section id="projects">
+        <p>Projects</p>
+      </section>
 
       <div className="cards">
-        <Card title="Mail Client" description="An intuitive email client that combines advanced filtering, sender-based message grouping, and AI-powered daily summaries for a seamless user experience." imgRoute={mail} />
-        <Card title="Ambulo" description="Ambulo is a Flutter app that recommends trails based on your profile and history, provides weather and trail alerts, allows group trip planning with shared equipment lists, and integrates GPX files for detailed navigation." imgRoute={commingSoon} />
+        <Card title="Mail Client" description="An intuitive email client that combines advanced filtering, sender-based message grouping, and AI-powered daily summaries for a seamless user experience." imgRoute={mail} link={'https://github.com/Danielbet21/Gmail_Assistant'} />
+        <Card title="Ambulo" description="Ambulo is a Flutter app that recommends trails based on your profile and history, provides weather and trail alerts, allows group trip planning with shared equipment lists, and integrates GPX files for detailed navigation." imgRoute={commingSoon} link={'https://github.com/Danielbet21/Ambulo'} />
 
       </div>
       
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,13 +1,14 @@
-
-function Card({title, description, imgRoute}) {
+function Card({ title, description, imgRoute, link }) {
   return (
-    <div className="card">
-      <img className="cardImg" src={imgRoute} alt="this is the first img"/>
-      <div className="overlay">
-        <h2 className="cardTitle">{title}</h2>
-        <p className="cardDescription">{description}</p>
+    <a href={link} target="_blank" rel="noopener noreferrer" className="cardLink">
+      <div className="card">
+        <img className="cardImg" src={imgRoute} alt="this is the first img" />
+        <div className="overlay">
+          <h2 className="cardTitle">{title}</h2>
+          <p className="cardDescription">{description}</p>
+        </div>
       </div>
-    </div>
+    </a>
   );
 }
 
